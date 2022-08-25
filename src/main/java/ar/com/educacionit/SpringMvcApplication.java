6496af0dfa28aa10c142a5c90aa5c0d12d18fd08
carlos horacio lopez <krloss@gmail.com>
2022-08-24 22:05:20 -0300

    add first controller

--- src/main/java/ar/com/educacionit/SpringMvcApplication.java
+++ src/main/java/ar/com/educacionit/SpringMvcApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMvcApplication {
 
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
 
---
+++ src/main/java/ar/com/educacionit/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package ar.com.educacionit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//@Repository
+//@Service => transaction
+@Controller
+public class HomeController {
+
+	//GET > http://localhost:8080/
+	
+	@GetMapping(value = "/")
+	public String home() {
+		//quiero ir  a la pagina home.html
+		return "home";
+	}
+}
