245fe65d704a4c1ad1ccd4a035371e4107453f73
carlos horacio lopez <krloss@gmail.com>
2022-08-31 22:02:16 -0300

    entity+reposiroty+service+controller+template

---
+++ src/main/java/ar/com/educacionit/controllers/SociosController.java
@@ -0,0 +1,29 @@
+package ar.com.educacionit.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ar.com.educacionit.domain.Socios;
+import ar.com.educacionit.services.SociosService;
+
+@Controller
+@RequestMapping("/socio")
+public class SociosController {
+
+	//D.I
+	@Autowired
+	private SociosService sociosService;
+	
+	//http://localhost:8081/socio/all
+	@GetMapping("/all")
+	public String all(Model model) {
+		List<Socios> socios = this.sociosService.buscarTodos();
+		model.addAttribute("socios", socios);
+		return "socios";
+	}
+}
---
+++ src/main/java/ar/com/educacionit/services/SociosService.java
@@ -0,0 +1,21 @@
+package ar.com.educacionit.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.educacionit.domain.Socios;
+import ar.com.educacionit.repository.SociosRepository;
+
+@Service
+public class SociosService {
+
+	//D.I
+	@Autowired
+	private SociosRepository repository;
+
+	public List<Socios> buscarTodos() {
+		return this.repository.findAll();
+	}
+}
