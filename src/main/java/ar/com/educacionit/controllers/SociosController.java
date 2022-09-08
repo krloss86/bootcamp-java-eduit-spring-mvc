a84c433abd40b8d9c6bb513d778beb91b57ab537
carlos horacio lopez <krloss@gmail.com>
2022-09-07 22:06:40 -0300

    ADD cupones

--- src/main/java/ar/com/educacionit/controllers/SociosController.java
+++ src/main/java/ar/com/educacionit/controllers/SociosController.java
@@ -1,15 +1,23 @@
 package ar.com.educacionit.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import ar.com.educacionit.domain.Socios;
 import ar.com.educacionit.services.SociosService;
@@ -53,4 +61,25 @@ public class SociosController {
 		return "edit";
 	}
 	
+	@PostMapping("/edit")
+	public String editar(
+			@Valid @ModelAttribute(name="SOCIO") Socios socio,
+			BindingResult result,
+			Model modelAndView) {
+		
+		//evaluar las validaciones
+		
+		//ModelAndView modelAndView  = new ModelAndView("edit");
+		
+		if(result.hasErrors()) {
+			modelAndView.addAttribute("SOCIO", socio);
+			return "edit";
+		}
+		
+		
+		//modelAndView.addObject("SOCIO", socio);
+		
+		//return modelAndView;
+		return "edit";
+	}
 }
